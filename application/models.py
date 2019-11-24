ec60d28115659fc61dbc21e75acb9f1b3edf12e1
Roni Lindholm <roni.lindholm@gmail.com>
2019-11-24 21:55:34 +0200

    Create basic classes for Book and Bookmark
    WIP: Model fields needs to be defined

---
+++ application/models.py
@@ -0,0 +1,19 @@
+from application.app import db
+
+
+class Base(db.Model):
+
+    __abstract__ = True
+
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+                              onupdate=db.func.current_timestamp())
+
+
+class Bookmark(Base):
+    pass
+
+
+class Book(Bookmark):
+    pass
