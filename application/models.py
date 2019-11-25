48275632095cc8bf9f14d5dd15dea73ad0e6a2d6
Roni Lindholm <roni.lindholm@gmail.com>
2019-11-25 09:03:28 +0200

    Implement database models for basic bookmark types

--- application/models.py
+++ application/models.py
@@ -12,8 +12,61 @@ class Base(db.Model):
 
 
 class Bookmark(Base):
-    pass
+    __tablename__ = 'bookmark'
+
+    read_status = db.Column(db.Boolean, default=False)
+    read_date = db.Column(db.DateTime)
+    header = db.Column(db.String(50))
+    comment = db.Column(db.String(1024))
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'bookmark'
+    }
 
 
 class Book(Bookmark):
-    pass
+    __tablename__ = 'book'
+
+    id = db.Column(db.Integer, db.ForeignKey('bookmark.id'), primary_key=True)
+    ISBN = db.Column(db.String(17))
+    writer = db.Column(db.String(250))
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'book',
+    }
+
+
+class Blog(Bookmark):
+    __tablename__ = 'blog'
+
+    id = db.Column(db.Integer, db.ForeignKey('bookmark.id'), primary_key=True)
+    URL = db.Column(db.String(250))
+    writer = db.Column(db.String(250))
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'blog',
+    }
+
+
+class Podcast(Bookmark):
+    __tablename__ = 'podcast'
+
+    id = db.Column(db.Integer, db.ForeignKey('bookmark.id'), primary_key=True)
+    name = db.Column(db.String(250))
+    description = db.Column(db.String(1024))
+    author = db.Column(db.String(250))
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'podcast',
+    }
+
+
+class Video(Bookmark):
+    __tablename__ = 'video'
+
+    id = db.Column(db.Integer, db.ForeignKey('bookmark.id'), primary_key=True)
+    URL = db.Column(db.String(250))
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'video',
+    }
