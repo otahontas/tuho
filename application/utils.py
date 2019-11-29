25c0004600986e31b9c65b531e6b1f9c6cc5c8aa
Roni Lindholm <roni.lindholm@gmail.com>
2019-11-29 10:27:09 +0200

    Add utility functions to resolve book details from ISBN

---
+++ application/utils.py
@@ -0,0 +1,60 @@
+import re
+
+import requests
+
+
+API_URL = 'https://www.googleapis.com/books/v1/volumes'
+
+
+def resolve_book_details(ISBN):
+    """
+    Return dict containing author and book title based on ISBN.
+
+    Args:
+        ISBN: Book ISBN-numer as string without hyphens, containin 13 numbers
+    Returns:
+        Dict containign keys author, title and ISBN or None if book not found
+    """
+    if not is_valid_isbn(ISBN):
+        raise ValueError(f"Inavlid ISBN {ISBN}")
+
+    data = _get_book_details(ISBN)
+
+    book = data["items"][0]["volumeInfo"]
+    authors = book.get("authors", None)
+    if authors:
+        authors = ', '.join(authors)
+
+    return {
+        "author": authors,
+        "title": book["title"],
+        "ISBN": ISBN
+    }
+
+
+def is_valid_isbn(ISBN):
+    """ Return True if given value is a valid ISBN number containing only numbers """
+    if not isinstance(ISBN, str):
+        print("not str")
+        return False
+
+    if not re.search('^[0-9]+$', ISBN):
+        print("regex")
+        return False
+
+    print("len", len(ISBN))
+    return len(ISBN) == 10 or len(ISBN) == 13
+
+
+def _get_book_details(ISBN):
+    """ Get book details by ISBN from API """
+
+    response = requests.get(f'{API_URL}?q=isbn{ISBN}')
+
+    if response.status_code != 200:
+        raise RuntimeError("Api request to resolve book details failed")
+
+    data = response.json()
+    if data["totalItems"] == 0:
+        return None
+    return data
