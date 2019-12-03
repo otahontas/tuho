20eaa82f96b1f82b80912d62d6c0b7b150ade37e
Roni Lindholm <roni.lindholm@gmail.com>
2019-12-03 19:32:53 +0200

    Fix flake8 error

--- application/views.py
+++ application/views.py
@@ -57,7 +57,9 @@ def get_bookmark(bookmark_id):
         return render_template("bookmarks/book.html", book=bookmark)
     elif bookmark.type == Bookmark.TYPE_VIDEO:
         yt = "https://www.youtube-nocookie.com/embed/"
-        embed = re.sub('http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)(&(amp;)?[\w\?=]*)?', yt, bookmark.URL) # substitute non-ID part with embed-URL
+        # substitute non-ID part with embed-URL
+        embed = re.sub(r'http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)' +
+                       r'(&(amp;)?[\w\?=]*)?', yt, bookmark.URL)
         return render_template("bookmarks/video.html", video=bookmark, embed=embed)
 
     abort(404)
