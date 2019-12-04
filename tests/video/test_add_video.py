31e369af1ebe6e7e14d930ca68c3ce68d38f9d58
Roni Lindholm <roni.lindholm@gmail.com>
2019-12-04 09:12:14 +0200

    Add tests for adding video

---
+++ tests/video/test_add_video.py
@@ -0,0 +1,21 @@
+from application.models import Video
+
+
+def test_video_insertion(client):
+    resp = client.post('/bookmarks/video', data={'header': 'test', 'URL': "",
+                       'comment': 'comment'}, follow_redirects=True)
+    assert resp.status_code == 200
+
+    video = Video.query.one()
+    assert video.header == "test"
+    assert video.URL == ""
+    assert video.comment == "comment"
+
+
+def test_add_video_form(client):
+    resp = client.get("/bookmarks/new/video")
+    assert resp.status_code == 200
+
+    data = str(resp.data)
+
+    assert '<input type="submit" value="Add a new video"/>' in data
