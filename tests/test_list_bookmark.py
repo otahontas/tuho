b3b79f1f31852d5d59bcaba65bd375247320f18a
jouni tenhunen <jouni.tenhunen@gmail.com>
2019-12-08 11:56:41 +0200

    Add read status filtering tests

--- tests/test_list_bookmark.py
+++ tests/test_list_bookmark.py
@@ -17,3 +17,15 @@ def test_no_books_shown_when_filtering_with_videos(client, book):
     response = client.get('/list?type=4')
     assert response.status_code == 200
     assert b'No bookmarks' in response.data
+
+def test_no_books_shown_when_filtering_with_seen_status_true(client, book):
+    response = client.get('/list?seen=1')
+    assert response.status_code == 200
+    assert b'No bookmarks' in response.data
+
+def test_book_shown_when_filtering_with_seen_status_true(client, book):
+    response = client.get('/list?seen=2')
+    assert response.status_code == 200
+
+    test_pattern = f"{book.header}"
+    assert test_pattern.encode() in response.data
